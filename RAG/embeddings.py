8307a9019e4a0d0b479ebd59006f5c57a0ec475d
Indrapalsingh8241 <thakurindrapal09@gmail.com>
2026-06-01 16:40:30 +0530

    Fix HuggingFace embeddings

--- RAG/embeddings.py
+++ RAG/embeddings.py
@@ -1,11 +1,8 @@
 from langchain_huggingface import HuggingFaceEndpointEmbeddings
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
-hf_token = os.getenv("HUGGINGFACE_API_TOKEN")
 embeddings = HuggingFaceEndpointEmbeddings(
-    repo_id="sentence-transformers/all-MiniLM-L6-v2",
-    hf_token = os.getenv("HUGGINGFACE_API_TOKEN")
+    repo_id="sentence-transformers/all-MiniLM-L6-v2"
 )
